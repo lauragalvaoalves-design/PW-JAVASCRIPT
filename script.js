0c8292fc1922dd890618d68f20c94aff23ea9d0a
Laura Alves Galvão <lauragalvaoalves@icloud.com>
2026-03-31 16:16:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var objpf = {
+    nome: "Julia",
+    idade: 16,
+    profissao: "Programadora",
+    estatrabalhando: false,
+
+};
+
+
+console.log(typeof objpf.estatrabalhando);
+
+
+
+console.log(objpf.nome);
+
+console.log(objpf.idade);
+
+objpf.estatrabalhando="true";
+console.log(objpf.estatrabalhando);
+
+objpf.nome="Gui";
+console.log(objpf.nome);
+
+console.log(objpf);
